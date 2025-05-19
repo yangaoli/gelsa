ee2e1039414e119b98d2cb3d167132335199b722
yangaoli <yang98913745@163.com>
2025-05-19 19:00:28 +0800

    Initial commit

---
+++ CPU_command.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y wget sudo python3.10 python3.10-dev python3-pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+pip install scipy statsmodels pandas numpy argparse
+
+sudo pip uninstall lsa
+sudo rm -rf /usr/local/lib/python3.10/dist-packages/lsa-1.0.2-py3.10.egg
+sudo rm -f /usr/local/bin/lsa_compute /usr/local/bin/m
+
+cd ./gelsa/
+sudo rm -rf build/ dist/ lsa.egg-info/
+
+cd ./Cpu_compcore/
+
+make
+
+cd ../
+sudo pip install .   # setup.py自动识别
+cd ../
+
+python in_out_data.py
+lsa_compute test.txt result -d 10 -r 1 -s 50 -p theo -T 0.1
